bfc4363cc2fe1a101df1d819a25e64ecc712bc13
Tanya Saroha <tanyasaroha93@gmail.com>
2022-09-15 00:40:34 +0530

    Added Rewards service and User class

---
+++ app/services/rewards/user.rb
@@ -0,0 +1,15 @@
+module Rewards
+  class User
+    attr_reader :identifier
+    attr_accessor :status, :points, :parent
+
+    STATUSES = %w[joined invited accepted].freeze
+
+    def initialize(identifier = nil, parent = nil, status = "joined")
+      @identifier = identifier
+      @points = 0
+      @parent = parent
+      @status = status
+    end
+  end
+end
